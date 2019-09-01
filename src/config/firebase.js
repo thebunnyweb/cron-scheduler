8e64b926ba89333b2fe75299232b237984d55e77
Harsh <hershd1@icloud.com>
2019-09-02 00:34:16 +0530

    SCRIPT-DEV: Branch Merge Commit

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,13 +2,7 @@ import firebase from 'firebase';
 
 
 var firebaseConfig = {
-    apiKey: "AIzaSyBN3-VAz4Ena3KdswTzhr5R4W7FioRwlrI",
-    authDomain: "cronscheduler-d6bc7.firebaseapp.com",
-    databaseURL: "https://cronscheduler-d6bc7.firebaseio.com",
-    projectId: "cronscheduler-d6bc7",
-    storageBucket: "cronscheduler-d6bc7.appspot.com",
-    messagingSenderId: "581990439823",
-    appId: "1:581990439823:web:68a9f3adc180cb8d"
+  
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
--- src/jobs/mycoso/dbbackup/backup.js
+++ src/jobs/mycoso/dbbackup/backup.js
@@ -1,21 +1,21 @@
-(function(){
-    
-    var main = function(data){
+(function () {
+
+    var main = function (data) {
         console.log('I m the main', data)
     }
 
-    if(process.argv.length > 2){
-        let parameters =  process.argv.slice(2);
+    if (process.argv.length > 2) {
+        let parameters = process.argv.slice(2);
         let data = {}
-        parameters.forEach(function(item){
-            if(item.split("=").length > 1){
-               let pars = item.split("=")
-               data = {...data, [pars[0]]: pars[1] }
+        parameters.forEach(function (item) {
+            if (item.split("=").length > 1) {
+                let pars = item.split("=")
+                data = { ...data, [pars[0]]: pars[1] }
             }
         });
         main(data);
     }
-   
 
-    
+
+
 })()
